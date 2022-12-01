280e7713439503abd7407d96fb7709ef5ef9cdac
nahum <nahum@DESKTOP-4C02UIV>
2022-11-30 21:26:18 -0600

    Commit Nahun

---
+++ src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,29 @@
+
+package com.tienda;
+
+import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver () {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
